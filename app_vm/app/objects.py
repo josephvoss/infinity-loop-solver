c249f6adde8d2bedcfe139db70f0e4458b6efdbb
Joseph Voss <brosephvossenator@gmail.com>
2017-01-21 22:30:31 -0600

    Moved functions to modify internal storage to class declaration
    Resolves identifaction of shapes 5 and 0

--- app_vm/app/objects.py
+++ app_vm/app/objects.py
@@ -3,49 +3,51 @@ import numpy as np
 class Data_storage:
 
     """
-        Data Object
-        Controls passing of data from image detector to solver and main
+    Data Object
+    Controls passing of data from image detector to solver and main
 
-        Data Members
-            Shape type at location
-                mxn matrix with values 0-5
-                    0: Empty space
-                    1: Shape with 1
-                    2: Shape with 2 curved
-                    3: Shape with 2 straight
-                    4: Shape with 3
-                    5: Shape with 4
-            Required Points
-                Not points, lines
-                    Line counter for an mxn matrix
-                        m*2+1xn+1 shape
+    Data Members
+        Shape type at location
+            mxn matrix with values 0-5
+                0: Empty space
+                1: Shape with 1
+                2: Shape with 2 curved
+                3: Shape with 2 straight
+                4: Shape with 3
+                5: Shape with 4
+        Required Points
+            Not points, lines
+                Line counter for an mxn matrix
+                    m*2+1xn+1 shape
 
-                        4 zeros make 1 dot
-                        1 2 =>      1
-                        4 3     4       2
-                                    3
-                        1: m, n*2
-                        2: m+1, n*2+1
-                        3: m, n*2+2
-                        4: m, n*2+1
+                    4 zeros make 1 dot
+                    1 2 =>      1
+                    4 3     4       2
+                                3
+                    1: m, n*2
+                    2: m+1, n*2+1
+                    3: m, n*2+2
+                    4: m, n*2+1
 
-                Spaces are holes, 0's are lines, x's are edges, ? is undef
-                 x x x x ?
-                x 0 0 0 x
-                 0 0 0 0 ?
-                x 0 0 0 x
-                 0 0 0 0 ?
-                x 0 0 0 x
-                 x x x x ?
+            Spaces are holes, 0's are lines, x's are edges, ? is undef
+             x x x x ?
+            x 0 0 0 x
+             0 0 0 0 ?
+            x 0 0 0 x
+             0 0 0 0 ?
+            x 0 0 0 x
+             x x x x ?
 
-                    
+            Value of 1 means points required here
+            Value of -1 means points required not here
+            Value of 0 means unknown
 
-            Fixed points
-                mxn matrix
-                    0 for unknown
-                    1 for fixed
-                    2+ for number of possibilities
-                    -1 for empty (necessary? can get empty from other)
+        Fixed points
+            mxn matrix
+                0 for unknown
+                1 for fixed
+                2+ for number of possibilities
+                -1 for empty (necessary? can get empty from other)
     """
 
     def set_size(self,m,n):
@@ -58,11 +60,58 @@ class Data_storage:
         self.required_points = np.zeros((m*2+1,n+1))
         self.fixed_points = np.zeros((m,n))
 
+    def set_required_points(self, point, a, b, c, d, value):
+        """
+        Set required points in object
+
+        Input: Tuple point to be set
+        Input: Bool if point 1 is required
+        Input: Bool if point 2 is required
+        Input: Bool if point 3 is required
+        Input: Bool if point 4 is required
+        Input: Integer for value to be set to (1 for required, -1 for required not)
+        Input: Data_storage object to set points
+
+        """
+        x = point[0]; y = point[1]
+        if a:
+            self.required_points[x*2,y] = value
+        if b:
+            self.required_points[x*2+1,y+1] = value
+        if c:
+            self.required_points[x*2+2,y] = value
+        if d:
+            self.required_points[x*2+1,y] = value
+
+    def get_required_points(self,m,n):
+        """
+        Get shape from required points coordinates
+
+        Input: Integer m coordinate
+        Input: Integer n coordinate
+        Input: Data_storage object to get points from
+        Output: Array of Tuples with the grid spaces boarding the required lines
+                Either veritically touching or horizontally touching
+                Either length two or length one
+
+        """
+        if m % 2: #if m is even
+            
+            pass    #do something
+        else:
+            pass
+
+
     # -1 == edge of grid
     # -9 == artifact of data storage => ignore
     # 0 == unknown
     # 1 == fixed
     def setup_required_matrix(self):
+        """
+        Initializes the required points matrix in the notation shown above.
+        Needs to be called after set_size
+
+        """
         for i in range(self.required_points.shape[0]):
             for j in range(self.required_points.shape[1]):
                 if i == 0 or i == self.required_points.shape[0]-1:
@@ -75,3 +124,40 @@ class Data_storage:
                         self.required_points[i,j] = -1
                 elif j == self.required_points.shape[1]-1: #even rows
                     self.required_points[i,j] = -9
+
+    def set_fixed_points(self,x,y,value):
+        """
+        Wrapper function to set the required points matrix at the same time as
+        the fixed points matrix
+
+        Input: Integer x value
+        Input: Integer y value
+        Input: Integer value to set fixed points to (should be 0 or 1)
+
+        """
+
+        self.fixed_points[x,y] = value
+        if value == 1:
+            if self.shape_matrix[x,y] == 1:
+                #self.set_required_points((x,y), 1, 1, 1, 1, 1)
+                pass
+            elif self.shape_matrix[x,y] == 2:
+                #self.set_required_points((x,y), 1, 1, 1, 1, 1)
+                pass
+            elif self.shape_matrix[x,y] == 3:
+                #self.set_required_points((x,y), 1, 1, 1, 1, 1)
+                pass
+            elif self.shape_matrix[x,y] == 4:
+                #self.set_required_points((x,y), 1, 1, 1, 1, 1)
+                pass
+            elif self.shape_matrix[x,y] == 5:
+                print "Shape 5"
+                self.set_required_points((x,y), 1, 1, 1, 1, 1)
+                pass
+            elif self.shape_matrix[x,y] == 0:
+                print "Shape 0"
+                self.set_required_points((x,y), 1, 1, 1, 1, -1)
+                pass
+            else:
+                pass
+
--- app_vm/app/solver.py
+++ app_vm/app/solver.py
@@ -1,44 +1,6 @@
 import numpy as np
 import objects
 
-def set_required_points(point, a, b, c, d, data_storage):
-    """
-    Set required points in object
-
-    Input: Tuple point to be set
-    Input: Bool if point 1 is required
-    Input: Bool if point 2 is required
-    Input: Bool if point 3 is required
-    Input: Bool if point 4 is required
-    Input: Data_storage object to set points
-
-    """
-    x = point[0]; y = point[1]
-    if a:
-        data_storage.required_points[y*2,x] = 1
-    if b:
-        data_storage.required_points[y*2+1,x+1] = 1
-    if c:
-        data_storage.required_points[y*2+2,x] = 1
-    if d:
-        data_storage.required_points[y*2+1,x] = 1
-
-def get_required_points(m,n,data_storage):
-    """
-    Get shape from required points coordinates
-
-    Input: Integer m coordinate
-    Input: Integer n coordinate
-    Input: Data_storage object to get points from
-    Output: Array of Tuples with the grid spaces boarding the req. lines
-            Either veritically touching or horizontally touching
-            Either length two or length one
-
-    """
-    if m % 2: #if m is even
-        pass    #do something
-
-
 def solve(data_object):
     """
     Problem solver functions
@@ -62,14 +24,19 @@ def solve(data_object):
     # 2. ID fixed points at start
     # shape 5 means all points around it are required
     locations_5 = np.where(data_object.shape_matrix == 5)
-    for i in locations_5:
-        data_object.fixed_points[i[1],i[0]] = 1
-        set_required_points(i,1,1,1,1,data_object)
+    for i in range(len(locations_5[0])):
+        data_object.set_fixed_points(locations_5[0][i], locations_5[1][i], 1)
+    # shape 0 means all points around it are neg required
+    locations_0 = np.where(data_object.shape_matrix == 0)
+    for i in range(len(locations_0[0])):
+        data_object.set_fixed_points(locations_0[0][i], locations_0[1][i], 1)
 
-"""
     # corners
-    corners = storage.shape_matrix[[0,0,-1,-1],[0,-1,0,-1]]
-    if 
+    corners = storage.shape_matrix[[0,0,-1,-1],[0,-1,0,-1]] # 4x4 matrix
+    locations_corners = np.where(corners == 2)
+    for i in range(len(locations_corners[0])):
+        data_object.set_fixed_points(locations_corners[0][i], locations_corners[1][i], 1)
+    
 
     data_objects.shape_matrix
     # find shapes at corners
@@ -93,4 +60,3 @@ def solve(data_object):
     for i in range(data_object.required_points.shape[0]):
         for j in range(data_object.required_points.shape[1]):
             pass
-"""
